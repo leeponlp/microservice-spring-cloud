148d56c077d5aa15a5a09120739d29198c877902
za-liupeng <liupeng@zhongan.io>
2018-07-06 16:22:38 +0800

    示例新增

---
+++ microservice-retry/src/main/java/com/leepon/cloud/FeignFallbackApplication.java
@@ -0,0 +1,28 @@
+package com.leepon.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker //开启断路器
+public class FeignFallbackApplication {
+
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeignFallbackApplication.class, args);
+  }
+}
