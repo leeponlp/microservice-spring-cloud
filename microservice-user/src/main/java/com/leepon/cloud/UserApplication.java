9a65498d8f1c9256d544266ccdc4b9beea4f2c8a
za-liupeng <liupeng@zhongan.io>
2019-03-28 09:57:25 +0800

    update

--- microservice-user/src/main/java/com/leepon/cloud/UserApplication.java
+++ microservice-user/src/main/java/com/leepon/cloud/UserApplication.java
@@ -1,9 +1,11 @@
 package com.leepon.cloud;
 
+import com.github.xiaour.api_scanner.annotation.Sapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@Sapi(controllers = {"com.leepon.cloud.controller"})
 @SpringBootApplication
 @EnableEurekaClient
 public class UserApplication {
--- microservice-user/src/main/java/com/leepon/cloud/controller/UserController.java
+++ microservice-user/src/main/java/com/leepon/cloud/controller/UserController.java
@@ -2,36 +2,34 @@ package com.leepon.cloud.controller;
 
 import com.leepon.cloud.annotation.EncryptField;
 import com.leepon.cloud.repository.UserRepository;
-import com.leepon.cloud.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.leepon.cloud.entity.User;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+
+@RequestMapping("/user")
 @Slf4j
 @RestController
-public class UserController implements IUserService {
+public class UserController {
 
   @Autowired
   private UserRepository userRepository;
 
 
+  @RequestMapping(value = "/{id}",method = RequestMethod.GET)
   @EncryptField(includes = {"username"})
-  @Override
   public User findById(@PathVariable Integer id) {
     log.info("=====访问UserApp=====");
     return userRepository.findOne(id);
   }
 
 
-  @Override
+  @RequestMapping(value = "/list",method = RequestMethod.POST)
   public List<User> findUserAll() {
 
     return userRepository.findAll();
