e9a3f4f352dcfcba6ff88e54fdb34d4ce257321d
za-liupeng <liupeng@zhongan.io>
2019-03-28 10:17:53 +0800

    新增ql用例

---
+++ microservice-qlexpression/src/main/java/com/leepon/cloud/QlExpressionApplication.java
@@ -0,0 +1,12 @@
+package com.leepon.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlExpressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlExpressionApplication.class, args);
+	}
+}
